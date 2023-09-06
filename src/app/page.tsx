cc8b773a918918c84e60fe2fd7f9195629024027
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2023-09-06 11:43:27 -0700

    Add bubble animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,12 @@ export default function Home() {
       <div className="img-2"></div>
       <div className="h-80 bg-gradient-to-b from-emerald-200 from-25% via-sky-300 via-55% to-sky-500 to-100%  text-xl">My name is Sergei</div>
       <div className="img-1"></div>
-      <div id="background-wrap">
+      <div className="background-wrap">
         <div className="bubble x1"></div>
         <div className="bubble x2"></div>
         <div className="bubble x3"></div>
         <div className="bubble x4"></div>
         <div className="bubble x5"></div>
-        <div className="bubble x6"></div>
-        <div className="bubble x7"></div>
-        <div className="bubble x8"></div>
-        <div className="bubble x9"></div>
-        <div className="bubble x10"></div>
       </div>
     </main>
   )
