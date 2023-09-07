1229f460f7439513fad6a4fdab00fe08ab290b38
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2023-09-06 21:22:47 -0700

    Change images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 import "./globals.css";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/images/logo.png"
 
 export default function Home() {
   return (
     <main>
+      <header>
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="logo"
+            height={80}
+          />
+        </Link>
+      </header>
       <div className="img-1"></div>
       <div className="h-60 bg-gradient-to-b from-sky-200 from-25% via-sky-300 via-55% to-emerald-300 to-100% text-xl">My name is Sergei</div>
       <div className="img-2"></div>
       <div className="h-80 bg-gradient-to-b from-emerald-200 from-25% via-sky-300 via-55% to-sky-500 to-100%  text-xl">My name is Sergei</div>
-      <div className="img-1"></div>
+      <div className="img-3"></div>
       <div className="background-wrap">
         <div className="bubble x1"></div>
         <div className="bubble x2"></div>
