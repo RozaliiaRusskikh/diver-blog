d301ee816b5f1858a40145a997f490452b53960e
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2023-09-11 22:35:37 -0700

    Change images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
         </Link>
       </header>
       <div className="img-1"></div>
-      <div className="h-60 bg-gradient-to-b from-sky-200 from-25% via-sky-300 via-55% to-emerald-300 to-100% text-xl">My name is Sergei</div>
+      <div className="h-80 bg-gradient-to-b from-cyan-400 from-20% via-blue-600 via-65% to-cyan-600 to-90% text-xl">My name is Sergei</div>
       <div className="img-2"></div>
-      <div className="h-80 bg-gradient-to-b from-emerald-200 from-25% via-sky-300 via-55% to-sky-500 to-100%  text-xl">My name is Sergei</div>
+      <div className="h-80 bg-gradient-to-b from-cyan-500 from-5% via-blue-900 via-65% to-blue-950 to-100%  text-xl">My name is Sergei</div>
       <div className="img-3"></div>
       <div className="background-wrap">
         <div className="bubble x1"></div>
